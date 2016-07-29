1b8615274a89370c376134fa27fdd932cfe482a6
fadi <1181979Fa>
2016-07-29 21:10:48 +0200

    project for jsDav

---
+++ bin/www.js
@@ -0,0 +1,17 @@
+/*
+ * @package jsDAV
+ * @subpackage DAV
+ * @copyright Copyright(c) 2011 Ajax.org B.V. <info AT ajax.org>
+ * @author Mike de Boer <info AT mikedeboer DOT nl>
+ * @license http://github.com/mikedeboer/jsDAV/blob/master/LICENSE MIT License
+ */
+"use strict";
+
+var jsDAV = require("../node_modules/jsDAV/lib/jsdav");
+jsDAV.debugMode = true;
+var jsDAV_Locks_Backend_FS = require("../node_modules/jsDAV/lib/DAV/plugins/locks/fs");
+
+jsDAV.createServer({
+  node: __dirname + "../node_modules/jsDAV/test/assets",
+  locksBackend: jsDAV_Locks_Backend_FS.new(__dirname + "../node_modules/jsDAV/test/assets")
+}, 8000);
